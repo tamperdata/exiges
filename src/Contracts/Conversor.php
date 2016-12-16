8efc52c11b09cf6c868fbc294e818ec93f44f809
Tiago Perrelli <tiagoyg@gmail.com>
2016-12-16 07:25:43 -0300

    fix documentation

--- src/Contracts/Conversor.php
+++ src/Contracts/Conversor.php
@@ -5,20 +5,56 @@ namespace Uself\Exiges\Contracts;
 interface Conversor
 {
 	/**
-	* Convert from degrees Celsius to Fahrenheit
+	* Convert from Celsius to Fahrenheit
 	*
-	* @param   int    $int
-	* @return  string 10°F
+	* @param   int/decimal $number
+	* @return  string 	   10°F
 	* @throws  InvalidArgumentException
 	*/
-	public function celsiusToFahrenheit($int = 0);
+	public function celsiusToFahrenheit($number = 0);
 
 	/**
-	* Convert from degrees Fahrenheit to Celsius
+	* Convert from Celsius to Kelvin
 	*
-	* @param   int    $int
-	* @return  string 10°C
+	* @param   int/decimal $number
+	* @return  string 	   10°K
 	* @throws  InvalidArgumentException
 	*/
-	public function fahrenheitToCelsius($int = 0); 
+	public function celsiusToKelvin($number = 0); 	
+
+	/**
+	* Convert from Fahrenheit to Celsius
+	*
+	* @param   int/decimal $number
+	* @return  string 	   10°C
+	* @throws  InvalidArgumentException
+	*/
+	public function fahrenheitToCelsius($number = 0); 
+
+	/**
+	* Convert from Fahrenheit to Kelvin
+	*
+	* @param   int/decimal $number
+	* @return  string 	   10°K
+	* @throws  InvalidArgumentException
+	*/
+	public function fahrenheitToKelvin($number = 0);
+
+	/**
+	* Convert from Kelvin to Celsius
+	*
+	* @param   int/decimal $number
+	* @return  string 	   10°C
+	* @throws  InvalidArgumentException
+	*/
+	public function kelvinToCelsius($number = 0);
+
+	/**
+	* Convert from Kelvin to Fahrenheit
+	*
+	* @param   int/decimal $number
+	* @return  string 	   10°F
+	* @throws  InvalidArgumentException
+	*/
+	public function kelvinToFahrenheit($number = 0);	
 }
--- src/Conversor.php
+++ src/Conversor.php
@@ -8,30 +8,86 @@ use Uself\Exiges\Contracts\Conversor as ConversorContract;
 class Conversor implements ConversorContract
 {
 	/**
-	* Convert from degrees Celsius to Fahrenheit
+	* Convert from Celsius to Fahrenheit
 	*
-	* @param   int    $int
-	* @return  string 10°F
+	* @param   int/decimal $number
+	* @return  string 	   10°F
 	* @throws  InvalidArgumentException
 	*/
-	public function celsiusToFahrenheit($int = 0)
+	public function celsiusToFahrenheit($number = 0)
 	{
-		$fahrenheit = ( $int * ( 9 / 5 )) + 32;
+		$fahrenheit = ( $number * ( 9 / 5 )) + 32;
 
 		return $fahrenheit . '°F';
 	}
 
 	/**
-	* Convert from degrees Fahrenheit to Celsius
+	* Convert from Celsius to Kelvin
 	*
-	* @param   int    $int
-	* @return  string 10°C
+	* @param   int/decimal $number
+	* @return  string 	   10°K
 	* @throws  InvalidArgumentException
 	*/
-	public function fahrenheitToCelsius($int = 0)
+	public function celsiusToKelvin($number = 0)
 	{
-		$celsius = ( $int - 32 ) * ( 5 / 9 );
+		$kelvin = $number + 273.15;
+
+		return $kelvin . '°K';
+	}	
+
+	/**
+	* Convert from Fahrenheit to Celsius
+	*
+	* @param   int/decimal $number
+	* @return  string 	   10°C
+	* @throws  InvalidArgumentException
+	*/
+	public function fahrenheitToCelsius($number = 0)
+	{
+		$celsius = ( $number - 32 ) * ( 5 / 9 );
+
+		return round($celsius, 4) . '°C';
+	}
+
+	/**
+	* Convert from Fahrenheit to Kelvin
+	*
+	* @param   int/decimal $number
+	* @return  string 	   10°K
+	* @throws  InvalidArgumentException
+	*/
+	public function fahrenheitToKelvin($number = 0)
+	{
+		$kelvin = ( $number + 459.67 ) * ( 5 / 9 );
+
+		return round($kelvin, 4) . '°K';
+	}
+
+	/**
+	* Convert from Kelvin to Celsius
+	*
+	* @param   int/decimal $number
+	* @return  string 	   10°C
+	* @throws  InvalidArgumentException
+	*/
+	public function kelvinToCelsius($number = 0)
+	{
+		$celsius = $number - 273.15;
 
 		return $celsius . '°C';
+	}
+
+	/**
+	* Convert from Kelvin to Fahrenheit
+	*
+	* @param   int/decimal $number
+	* @return  string 	   10°F
+	* @throws  InvalidArgumentException
+	*/
+	public function kelvinToFahrenheit($number = 0)
+	{
+		$fahrenheit = ( $number  * ( 9 / 5 )) - 459.67;
+
+		return $fahrenheit . '°F';
 	}
 }
